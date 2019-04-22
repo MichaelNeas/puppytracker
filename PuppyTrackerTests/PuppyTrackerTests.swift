924633a8f6cc3ff7bfe299d382f86ea3a8f34047
michaelneas <michael.r.neas@gmail.com>
2019-04-21 22:37:18 -0400

    food -> pup

--- PuppyTrackerTests/PuppyTrackerTests.swift
+++ PuppyTrackerTests/PuppyTrackerTests.swift
@@ -10,32 +10,32 @@ import XCTest
 @testable import PuppyTracker
 
 class PuppyTrackerTests: XCTestCase {
-    //MARK: Meal Class Tests
-    //Confirm that the Puppy initializer returns a Meal object when passed valid params
+    //MARK: Pup Class Tests
+    //Confirm that the Puppy initializer returns a Pup object when passed valid params
     func testPuppyInitializationSucceeds(){
         // Zero rating
-        let zeroRatingMeal = Puppy.init(name: "Zero", photo: nil, rating: 0)
-        XCTAssertNotNil(zeroRatingMeal)
+        let zeroRatingPup = Puppy.init(name: "Zero", photo: nil, rating: 0)
+        XCTAssertNotNil(zeroRatingPup)
         
         // Highest positive rating
-        let positiveRatingMeal = Puppy.init(name: "Positive", photo: nil, rating: 5)
-        XCTAssertNotNil(positiveRatingMeal)
+        let positiveRatingPup = Puppy.init(name: "Positive", photo: nil, rating: 5)
+        XCTAssertNotNil(positiveRatingPup)
     }
     
     
     
-    // Confirm that the Meal initialier returns nil when passed a negative rating or an empty name.
+    // Confirm that the Pup initialier returns nil when passed a negative rating or an empty name.
     func testPuppyInitializationFails() {
         // Negative rating
-        let negativeRatingMeal = Puppy.init(name: "Negative", photo: nil, rating: -1)
-        XCTAssertNil(negativeRatingMeal)
+        let negativeRatingPup = Puppy.init(name: "Negative", photo: nil, rating: -1)
+        XCTAssertNil(negativeRatingPup)
         
         // Empty String
-        let emptyStringMeal = Puppy.init(name: "", photo: nil, rating: 0)
-        XCTAssertNil(emptyStringMeal)
+        let emptyStringPup = Puppy.init(name: "", photo: nil, rating: 0)
+        XCTAssertNil(emptyStringPup)
         
         // Rating exceeds maximum
-        let largeRatingMeal = Puppy.init(name: "Large", photo: nil, rating: 6)
-        XCTAssertNil(largeRatingMeal)
+        let largeRatingPup = Puppy.init(name: "Large", photo: nil, rating: 6)
+        XCTAssertNil(largeRatingPup)
     }
 }
